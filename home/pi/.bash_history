4fb96b50fdf9e526dbd821ef4bc54066a359c841
sirarsalih <sirars@gmail.com>
2014-11-09 12:12:13 +0100

    Initial commit.

---
+++ home/pi/.bash_history
@@ -0,0 +1,39 @@
+sudo apt-get update && sudo apt-get upgrade
+wget http://nodejs.org/dist/v0.10.2/node-v0.10.2-linux-arm.pi.tar.gz
+wget http://nodejs.org/dist/v0.10.2/node-v0.10.2-linux-arm-pi.tar.gz
+tar -xvzf node-v0.10.2-linux-arm-pi.tar.gz 
+node-v0.10.2-linux-arm-pi/bin/node --version 
+.bash_profile
+nano vi ~/.bash_aliases
+vi ~/.bash_aliases
+export PATH=$PATH:/usr/local/bin/node
+export PATH=$PATH:/usr/local/bin/npm
+npm --version
+export NODE_JS_HOME=/home/pi/node-v0.10.2-linux-arm-pi
+export PATH=$PATH:$NODE_JS_HOME/bin
+npm --version
+gcc --version
+npm install -g node-gyp
+npm install -g express
+npm install -g ar-drone
+npm install -g ardrone-autonomy
+ipconfig
+sudo ipconfig
+sudo ifconfig
+cat /etc/network/interfaces
+ifconfig
+sudo ifconfig
+netstat -nr
+sudo nano /etc/network/interfaces
+sudo reboot
+sudo pwd 19Eseret87
+exit
+sudo passwd
+exit
+passwd
+exit
+ls
+node node_server.js 
+sudo node node_server.js
+sudo update-rc.d ssh defaults
+sudo reboot
---
+++ home/pi/node_server.js
@@ -0,0 +1,68 @@
+               var backupMsg = "<h3 style=\"color:green;\">Last server backup successfully completed on "+new Date().toLocaleString()+"</h3>";
+var fs = require('fs');
+var https = require('https');
+var privateKey = fs.readFileSync('/home/pi/ssl_certificate/myserver.key', 'utf8');
+var certificate = fs.readFileSync('/home/pi/ssl_certificate/raspi_sirars_com.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+var express = require('/home/pi/node-v0.10.2-linux-arm-pi/lib/node_modules/express');
+var app = express();
+var ip = "192.168.1.110"; //RasPi IP
+var port = 443;           //HTTPS
+//var port = 80;          //HTTP
+var responseText = "";
+var arDroneAutonomy = require('/home/pi/node-v0.10.2-linux-arm-pi/lib/node_modules/ardrone-autonomy');
+var arDrone = require('/home/pi/node-v0.10.2-linux-arm-pi/lib/node_modules/ar-drone');
+var welcomeMsg = "<h1>Sirar's RaspberryPi Node Server</h1>";
+var sslLogo = '<a href=\"https://www.positivessl.com\" style=\"font-family: arial; font-size: 10px; color: #212121; text-decoration: none;\"><img src=\"https://www.positivessl.com/images-new/PositiveSSL_tl_trans2.png\" alt=\"SSL Certificate\" title=\"SSL Certificate\" border=\"0\" /></a><br>';
+
+app.get('/', function(request, response){
+    response.send(welcomeMsg+backupMsg+sslLogo);
+});
+
+app.get('/fly', function(request, response){
+
+  var client = arDrone.createClient();
+
+  client.takeoff();
+
+  client
+    .after(5000, function() {
+      this.clockwise(0.5);
+    })
+    .after(3000, function() {
+      this.stop();
+      this.land();
+    });
+    var coordinates = request.query.c;
+    var mission  = arDroneAutonomy.createMission();
+    responseText = welcomeMsg+backupMsg+sslLogo+"Preparing drone flying mission...<br>";
+    console.log('Preparing drone flying mission...');
+    //mission.takeoff()
+    //.go({x:0, y:0, z:1.5})
+    //.land();
+    responseText += "Drone taking off...<br>";
+    console.log("Drone taking off...");
+    mission.run(function (err, result) {
+        if (err) {
+            responseText += "Flying mission failed!";
+            console.log("Flying mission failed!");
+            mission.client().stop();
+            mission.client().land();
+            response.send(responseText);
+        } else {
+          if(coordinates != undefined) {
+	      for(var i = 0; i < coordinates.length; i++) {
+                responseText += "Drone flew to position "+coordinates[i]+".<br>";
+                console.log("Drone flew to position "+coordinates[i]+".");
+            }
+          }
+            responseText += "Drone landed.";
+            console.log("Drone landed.");
+            response.send(responseText);
+        }
+    });
+});
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port, ip);
+//app.listen(port, ip);
+console.log('Node express server started on port %s', port);
