1994be88b22ffb37335871cb1ad5f11104f5d0b4
sirarsalih <sirars@gmail.com>
2015-05-15 15:44:15 +0200

    Corrected file path.

--- home/pi/twitter/if_hackathon_2015/advanced_twitter_bot.sh
+++ home/pi/twitter/if_hackathon_2015/advanced_twitter_bot.sh
@@ -1,4 +1,4 @@
 NODE_JS_HOME=/home/pi/node-v0.10.2-linux-arm-pi
 PATH=$PATH:$NODE_JS_HOME/bin
-node /home/pi/twitter/if_hackathon_2015/authenticate_and_tweet.js
+node /home/pi/twitter/if_hackathon_2015/advanced_twitter_bot.js
 
