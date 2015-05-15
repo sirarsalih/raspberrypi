e44c07c513aabb3b1f1fa28688d88304bfedca40
sirarsalih <sirars@gmail.com>
2015-05-15 08:26:59 +0200

    Restructured folders.

--- home/pi/twitter/user_groups/authenticate_and_tweet.sh
+++ home/pi/twitter/user_groups/authenticate_and_tweet.sh
@@ -1,3 +1,3 @@
 NODE_JS_HOME=/home/pi/node-v0.10.2-linux-arm-pi
 PATH=$PATH:$NODE_JS_HOME/bin
-node /home/pi/twitter/authenticate_and_tweet.js
+node /home/pi/twitter/user_groups/authenticate_and_tweet.js
